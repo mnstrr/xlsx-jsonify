eee40c85f06196d2a16e05161452f20dd2ceadac
Felix Winkelmann <felixwinkelmann@hotmail.com>
2016-07-04 22:54:10 +0200

    finalize recursive json k-v search

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from searchAndReplace import SearchAndReplace as Sar
 class Main:
     xlxs_doc = 'translation2.xlsx'
     json_doc = 'translation.json'
+    json_doc_new = 'translation_new.json'
     sheet_name = 'sheet1'
     blacklist = ['']
 
     def __init__(self):
-        sar = Sar(self.xlxs_doc, self.json_doc, self.sheet_name, self.blacklist)
+        sar = Sar(self.xlxs_doc, self.json_doc,self.json_doc_new, self.sheet_name, self.blacklist)
 
 
 start_time = time.time()
--- searchAndReplace.py
+++ searchAndReplace.py
@@ -1,71 +1,81 @@
 import openpyxl
 import json
-import pp
+import ftfy
 
 
 class SearchAndReplace:
-    def __init__(self, xlsx_doc, json_doc, sheet_name, blacklist):
+    def __init__(self, xlsx_doc, json_doc, json_doc_new, sheet_name, blacklist):
         print('init SearchAndReplace')
         self.__xlsx_doc = xlsx_doc
         self.__json_doc = json_doc
+        self.__json_doc_new = json_doc_new
         self.__sheet_name = sheet_name
         self.__blacklist = blacklist
 
-        self.__replaceJson()
-        # self.__crawlExcel()
+        self.__data = ""
+        self.__newdata = ""
 
-    def __crawlExcel(self):
+        self.__replaceJson()
+        # self.__main()
+
+    def __main(self):
+        f = open(self.__json_doc, 'r')
+        self.__data = f.read()
+        f.close()
+
+        self.__newdata = self.__data
+
+        self.__crawlexcel()
+
+        print(self.__newdata)
+
+        f = open(self.__json_doc_new, 'w')
+        f.write(self.__newdata)
+        f.close()
+
+    def __crawlexcel(self):
         print('start search')
 
         wb = openpyxl.load_workbook(self.__xlsx_doc)
         sheet = wb.get_sheet_by_name(self.__sheet_name)
 
         for rowNum in range(2, sheet.max_row):  # skip the first row
-            searchTerm = sheet.cell(row=rowNum, column=5).value
-            transTerm = sheet.cell(row=rowNum, column=6).value
-            if searchTerm is not None and transTerm is not None:
+            text = sheet.cell(row=rowNum, column=5).value
+            translation = sheet.cell(row=rowNum, column=6).value
+            if text is not None and translation is not None:
                 # print(searchTerm)
 
-                print(transTerm)
+                print(text, translation)
+                self.___naivereplacement(text, translation)
 
     def __replaceJson(self):
-        testStr = "Continue with this browser"
+        testStr = "i-Dos"
+        testStr2 = ftfy.fix_encoding(testStr)
         replaceStr = "TROLOLO"
         json_data = open('data.json')
         jdata = json.load(json_data)
-        # self.__fixup(jdata,testStr,replaceStr)
-        # print(type(jdata))
 
-        # print(jdata.keys())
-        #
-        # for key in jdata.keys():
-        #     print(type(jdata[key]))
-        # # pp(jdata)
+        self.__checkdict(jdata, testStr2, replaceStr)
 
-        self.__fixup2(jdata, testStr, replaceStr)
+    def __checkdict(self, collection, k, v):
+        for key in collection.keys():
+            self.__checkitem(collection[key], k, v)
 
-        print("REPLACED")
+    def __checklist(self, collection, k, v):
+        for item in collection:
+            self.__checkitem(item, k, v)
 
-    def __fixup2(self, dict, k, v):
-        for key in dict.keys():
-            if dict[key] == v:
-                print("FOUND YOU", key)
-                break
-            elif (type(dict[key]) is dict):
-                self.__fixup2(dict[key], k, v)
-            elif type(dict[key] is list):
-                self.__fixupList(dict[key],k,v)
+    def __checkitem(self, item, k, v):
+        if type(item) is str:
+            if ftfy.fix_encoding(item) == k:
+              self.__replaceitem(item)
+        if type(item) is dict:
+            self.__checkdict(item, k, v)
+        if type(item) is list:
+            self.__checklist(item, k, v)
 
+    def __replaceitem(self, item):
+        print("FOUND YOU: ", ftfy.fix_encoding(item))
 
-    def __fixupList(self,list,k,v):
-        for item in list:
-            print(item)
-
-    def __fixup(self, adict, k, v):
-        for key in adict.keys():
-            print(key)
-            if adict[key] == v:
-                adict[key] = v
-                break
-            elif type(adict[key]) is dict:
-                self.__fixup(adict[key], k, v)
+    def ___naivereplacement(self, text, translation):
+        self.__newdata = self.__newdata.replace(text, translation)
